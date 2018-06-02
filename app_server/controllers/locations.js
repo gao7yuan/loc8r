fdeb158d6838e136515f27896ee295d33869b74e
gao7yuan <gao7yuan@gmail.com>
2018-06-01 23:43:49 -0700

    added real cafe nearby

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,9 +8,9 @@ const homelist = function(req, res) {
     },
     sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
     locations: [{
-        name: 'Starcups',
-        address: '125 High Street, Reading, RG6 1PS',
-        rating: 3,
+        name: 'HodgePodge Cafe',
+        address: '6016 Bothell Way NE Suite J, Kenmore, WA 98028',
+        rating: 5,
         facilities: ['Hot drinks', 'Food', 'Premium wifi'],
         distance: '100m'
     }, {
@@ -32,46 +32,41 @@ const homelist = function(req, res) {
 /*GET 'Location infor' page */
 const locationInfo = function(req, res) {
   res.render('location-info', {
-    title: 'Starcups',
+    title: 'HodgePodge Cafe',
     pageHeader: {
-      title: 'Starcups'
+      title: 'HodgePodge Cafe'
     },
     sidebar: {
       context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
       callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
     },
     location: {
-      name: 'Starcups',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 3,
+      name: 'HodgePodge Cafe',
+      address: '6016 Bothell Way NE Suite J, Kenmore, WA 98028',
+      rating: 5,
       facilities: ['Hot drinks', 'Food', 'Premium wifi'],
       coords: {
-        lat: 51.455041,
-        lng: -0.9690884
+        lat: 47.7581963,
+        lng: -122.2652175
       },
       openingTimes: [{
-        days: 'Monday - Friday',
-        opening: '7:00am',
-        closing: '7:00pm',
+        days: 'Tuesday - Sunday',
+        opening: '11:00am',
+        closing: '8:00pm',
         closed: false
       }, {
-        days: 'Saturday',
-        opening: '8:00am',
-        closing: '5:00pm',
-        closed: false
-      }, {
-        days: 'Sunday',
+        days: 'Monday',
         closed: true
       }],
       reviews: [{
-        author: 'Simon Holmes',
+        author: ' Tovelo God ',
         rating: 5,
-        timestamp: '16 July 2013',
-        reviewText: 'What a great place. I can\'t say enough good things about it.'
+        timestamp: '16 May 2018',
+        reviewText: 'Coffee is good. Wifi is good. Love the food too. I can\'t say enough good things about it.'
       }, {
-        author: 'Charlie Chaplin',
+        author: ' Shroud Grzeseik ',
         rating: 3,
-        timestamp: '16 June 2013',
+        timestamp: '13 May 2018',
         reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
       }]
     }
@@ -82,7 +77,7 @@ const locationInfo = function(req, res) {
 const addReview = function(req, res) {
   res.render('location-review-form', {
     title: 'Review Starcups on Loc8r',
-    pageHeader: { title: 'Review Starcups' } 
+    pageHeader: { title: 'Review Starcups' }
   });
 };
 
