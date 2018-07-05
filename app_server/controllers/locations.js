4da840ebb3582939445cf90e3ccd194bd5b23157
gao7yuan <gao7yuan@gmail.com>
2018-07-04 21:38:56 -0700

    ch7 add request module

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,3 +1,11 @@
+const request = require('request');
+const apiOptions = {
+  server: 'http://localhost:3000'
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = 'https://loc8r-yuangao.herokuapp.com';
+}
+
 /* GET 'home' page */
 const homelist = function(req, res) {
   res.render('locations-list', {
