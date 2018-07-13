06f4115e6e09b97f0dd85d50596eb867527925a9
gao7yuan <gao7yuan@gmail.com>
2018-07-12 22:14:18 -0700

    ch8 put into prod

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'loc8r-public')));
 
 app.use('/api', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
